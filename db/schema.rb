a5017db05eba16ffda5fe86e07aba8b6f4cf6a51
Clement Morisset <morissetcl87@gmail.com>
2016-11-28 16:15:28 +0100

    user view and user controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128111504) do
+ActiveRecord::Schema.define(version: 20161128131537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20161128111504) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "age"
+    t.string   "city"
+    t.string   "country"
+    t.string   "religion"
+    t.text     "biography"
+    t.string   "language"
+    t.string   "gender"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
