a0c0bd607a961964742959125fb940ebef21e07f
Clement Morisset <morissetcl87@gmail.com>
2016-11-28 19:17:38 +0100

    update in progress, create new done

--- app/controllers/prayers_controller.rb
+++ app/controllers/prayers_controller.rb
@@ -1,26 +1,33 @@
 class PrayersController < ApplicationController
 
 	def new
-		@prayer = Prayer.new(params[:pain_id])
+		@pain = Pain.find(params[:pain_id])
+		@prayer = Prayer.new
 	end
 
 	def create
-		@prayer = Prayer.new(prayer_params)
 		@pain = Pain.find(params[:pain_id])
+		@prayer = Prayer.new(prayer_params)
+		@prayer.user = current_user
 		@prayer.pain = @pain
-		@prayer.user == current_user
 		 if @prayer.save
       	redirect_to pain_path(@pain)
     	else
       	flash.now[:alert] = "You didn't fill the form correctly"
-      	render "pains/show"
+      	render :new
     	end
 	end
 
 	def edit
+		@pain = Pain.find(params[:pain_id])
+		@prayer = Prayer.find(params[:id])
 	end
 
 	def update
+		@pain = Pain.find(params[:pain_id])
+		@prayer = Prayer.find(params[:prayer_id])
+		@prayer.update(prayer_params)
+		redirect_to pain_path(@pain)
 	end
 
 	def destroy
@@ -29,7 +36,11 @@ class PrayersController < ApplicationController
 	private
 
 	def set_prayer
-		
+		@prayer = Prayer.find(params[:id])
+	end
+
+	def set_pain
+		@pain = Pain.find(params[:pain_id])
 	end
 
 	def prayer_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit, :update]
   resources :pains do
-    resources :prayers, only: [:new, :create]
+    resources :prayers, only: [:new, :create, :edit, :update]
   end
 
 end
+ 
