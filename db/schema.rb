f45efb297fa28fdee9226a67bcf084ef18c9c8de
Cyrielle Faure <cyrielle.faure@wanadoo.fr>
2016-12-01 11:08:31 +0100

    autocomplete added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130094521) do
+ActiveRecord::Schema.define(version: 20161130114928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20161130094521) do
     t.integer  "pain_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "audio"
     t.index ["pain_id"], name: "index_prayers_on_pain_id", using: :btree
     t.index ["user_id"], name: "index_prayers_on_user_id", using: :btree
   end
