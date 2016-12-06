3b926f44eb8f006fd585cd39cb854714c99dc343
Clement Morisset <morissetcl87@gmail.com>
2016-12-06 16:11:18 +0100

    bug fixed

--- app/models/prayer.rb
+++ app/models/prayer.rb
@@ -2,12 +2,18 @@ class Prayer < ApplicationRecord
   include PublicActivity::Model
   tracked owner: Proc.new { |controller, model| controller.current_user }
 
-	mount_uploader :audio, AudioUploader
+  mount_uploader :audio, AudioUploader
 
   belongs_to :user
   belongs_to :pain
 
-  validates :title, presence: true, allows_blank: false;
-  validates :description, presence: true, allows_blank: false;
+  validates :title, presence: true, allows_blank: false
+  validates :description, presence: true, allows_blank: false, if: :audio_blank?
   #validates :description, presence: true, allows_blank: false;
+
+private
+
+  def audio_blank?
+    audio.blank?
+  end
 end
