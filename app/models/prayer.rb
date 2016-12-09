928433ad5f4e7b5318ff4b3d4d66bd3bccea2971
Clement Morisset <morissetcl87@gmail.com>
2016-12-09 11:57:47 +0100

    Fix notifications bug

--- app/models/prayer.rb
+++ app/models/prayer.rb
@@ -1,5 +1,7 @@
 class Prayer < ApplicationRecord
   include PublicActivity::Model
+  has_many :activities, as: :trackable, class_name: 'PublicActivity::Activity', dependent: :destroy
+
   tracked owner: Proc.new { |controller, model| controller.current_user }
 
   mount_uploader :audio, AudioUploader
