32c6e279c484b933097d2ab54d1819bd9c8125f8
Clement Morisset <morissetcl87@gmail.com>
2016-12-29 00:15:43 +0100

    mailer seems to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "www.blessme.io" }
+  config.action_mailer.default_url_options = { host: "blessme.io" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
